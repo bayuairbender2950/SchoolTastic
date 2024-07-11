183d94ba62891392b430704322f82ca660d4a623
BayuAirBender <47329758+bayuairbender2950@users.noreply.github.com>
2024-07-11 15:36:31 +0800

    Update init.js

--- mongoose/init.js
+++ mongoose/init.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 import chalk from 'chalk';
 mongoose.connect(process.env.atlas).then(
-        console.log(chalk.yellow('[DATABASE]') + chalk.green(' Connected Successfully'))
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 10000 // 10 seconds timeout
+  console.log(chalk.yellow('[DATABASE]') + chalk.green(' Connected Successfully'))
 )
